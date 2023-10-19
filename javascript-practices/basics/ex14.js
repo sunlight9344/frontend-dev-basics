76369801e7ff690685537c1e7eaa5e409eff4c61
sunlight9344 <sunlight9344@gmail.com>
2023-10-19 13:16:16 +0900

    ex14

--- javascript-practices/basics/ex14.js
+++ javascript-practices/basics/ex14.js
@@ -13,6 +13,31 @@ var url = "https://www.mysite.com/user?name=둘리아모리진진모리장단모
 var url2 = escape(url);
 console.log(url2);
 
-// 2. encodeURL
+// 2. encodeURL: URL 전체 중 파라미터만 encoding 한다. url 전체를 encoding 해야 하는 경우(o)
+var url3 = encodeURI(url);
+console.log(url3);
 
-// 3. encodeURLComponent
+// 3. encodeURLComponent: URL 중에 값만 Encoding해야 하는 경우 사용한다.
+var url4 = encodeURIComponent(url);
+console.log(url4);
+
+// 4. encodingURIComponent 사용 예
+// 만들어야 하는 url: "http://www.mysite.com/user?name=둘리&email=dooly@gamil.com"
+
+var url = "http://www.mysite.com/user";
+var formData = {
+    name: "둘리",
+    email: "dooly@gmail.com"
+}
+
+var toQueryString = function(o) {
+    var qs = [];
+    for(prop in o) {
+        qs.push(prop + "=" + encodeURIComponent(o[prop]));
+    }
+
+    return qs.join("&")
+}
+
+console.log(url + "?" + toQueryString(formData));
+console.log(`${url}?${toQueryString(formData)}`);
